5748ce92e8484bb7b2422c7ff620e2f79fab68ed
AlifiaRahmanita <rahmanitaalif@gmail.com>
2023-06-11 23:46:16 +0700

    CI

---
+++ CodeIgniter/MI4BApp/app/Controllers/Film.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+//step1
+use App\Models\FilmModel;
+
+class Film extends BaseController
+{
+
+    //step2 prperti
+    protected $Film;
+    //step 3 buat fungsi construct untuk inisiasi clas model
+    public function __construct()
+    {
+        //step 4 panggil property film
+        $this->Film = new FilmModel();
+    }
+
+
+    public function index()
+    {
+        // //step 5 memanggil ulang
+        // dd($this->Film->getFilm());
+        //array
+        $data['data_film'] = $this->Film->getAllData();
+        return view("film/index", $data);
+        
+    }
+
+    public function all(){
+        $data['semuaFilm'] = $this->Film->getAllData();
+        return view("film/semuaFilm",$data);
+    }
+
+    public function film_by_id(){
+        dd($this->Film->getDataByID(1));
+    }
+
+    public function film_by_genre(){
+        dd($this->Film->getDataBy("Horror"));
+    }
+
+    public function film_order(){
+        dd($this->Film->getOrderBy());
+    }
+
+    public function film_limit_five(){
+        dd($this->Film->getLimit());
+    }
+}
---
+++ CodeIgniter/MI4BApp/app/Models/FilmModel.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class FilmModel extends Model
+{
+    protected $table            = 'film';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $allowFields      = [];
+
+    public function getFilm(){
+
+        $data =
+        [
+            [
+                "nama_film" => "Sewu Dino",
+                "genre" => "Horor",
+                "duration" => "1 jam 43 menit"
+            ],
+            [
+                "nama_film" => "Ivvana",
+                "genre" => "Horor",
+                "duration" => "1 jam 20 menit"
+            ],
+            [
+                "nama_film" => "Menjelang Maghrib",
+                "genre" => "Horor",
+                "duration" => "1 jam 30 menit"
+            ],
+            [
+                "nama_film" => "Upin Ipin The Movie",
+                "genre" => "Animasi",
+                "duration" => "1 jam 19 menit"
+            ],
+            [
+                "nama_film" => "Pragos Sang Iblis",
+                "genre" => "Animasi",
+                "duration" => "1 jam 05 menit"
+            ]
+        ];
+
+        return $data;
+    }
+    //findALL()
+    public function getAllData(){
+        return $this->findAll();
+     }
+    //find($id)
+     public function getDataByID($id){
+         return $this->find($id);
+     }
+     //where($column, $value)
+     public function getDataBy($data){
+         return $this->where("genre", $data)->findAll();
+     }
+    //orderBy($column, $order)
+     public function getOrderBy(){
+         return $this->orderBy("created_at","desc")->findAll();
+     }
+    //limit($limit, $offset)
+     public function getLimit(){
+         return $this->limit(5)->get()->getResultArray();
+    }
+
+}
