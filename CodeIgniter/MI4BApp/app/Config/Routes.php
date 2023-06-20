31f87e0c730d3fb8020bc12f6a2e03651827bea3
AlifiaRahmanita <rahmanitaalif@gmail.com>
2023-06-20 20:12:00 +0700

    mengubah routes
    agar saling terhubung

--- CodeIgniter/MI4BApp/app/Config/Routes.php
+++ CodeIgniter/MI4BApp/app/Config/Routes.php
@@ -29,7 +29,8 @@ $routes->setAutoRoute(True);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'film::all');
+// $routes->get('/', 'Home::index');
 
 /*
  * --------------------------------------------------------------------
