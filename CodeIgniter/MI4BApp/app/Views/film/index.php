39a0454d287d046f63637a79e4a459a45acbd3a5
AlifiaRahmanita <rahmanitaalif@gmail.com>
2023-07-06 00:22:43 +0700

    UPDATE DELETE

--- CodeIgniter/MI4BApp/app/Views/film/index.php
+++ CodeIgniter/MI4BApp/app/Views/film/index.php
@@ -1,48 +1,92 @@
-<?= $this->extend('layout/page_layout') ?>
+<?= $this->extend("layout/page_layout");
+echo $this->section("content"); ?>
 
-<?= $this->section('content') ?>
+<div class="row">
+  <div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
 
-    <div class="container">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <h1>Semua Film</h1>
-                            </div>
-                            <div class="col-md-6 text-end">
-                                <a href="/film/add" class="btn btn-primary">Tambah Data</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <table class="table table-hover">
-                <tr>
-                    <th>No</th>
-                    <th>Nama Film</th>
-                    <th>Cover</th>
-                    <th>Genre</th>
-                    <th>Duration</th>
-                    <th>Action</th>
-                </tr>
-                <?php $i =1;
-                 foreach($data_film as $film) :?>
-                <tr>
-                    <td><?= $i++; ?></td>
-                    <td><?= $film['nama_film']?></td>
-                    <td><img style="width: 50px;" src="/assets/cover/<?= $film["cover"]?>" class="card-img-top"></td>
-                    <td><?= $film['nama_genre']?></td>
-                    <td><?= $film['duration']?></td>
-                    <td>
-                    <a href="/film/update/<?= $film["id"]; ?>" class="btn btn-success">Update</a>
-                    <a href="" class="btn btn-warning">Delete</a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-            </table>
-            </div>
+        <div class="row">
+        <div class="col-md-6">
+            <h1>Semua Film</h1>
+        </div>
+        <div class="col-md-6 text-end">
+            <a href="/film/add" class="btn btn-primary">Tambah Data</a>
+        </div>
+    </div>
         </div>
     </div>
     
-    <?= $this->endSection() ?>
+  <table class="table table-hover">
+                <tr>
+                    <th>No.</th>
+                    <th>Cover</th>
+                    <th>Nama Film</th>
+                    <th>Genre</th>
+                    <th>Duration</th>
+                    <th>Action</th>
+                   
+                    
+                </tr>
+
+                <?php $i = 1;
+                foreach ($data_film as $film): ?>
+                    <tr>
+                        <td>
+                            <?= $i++; ?>
+                        </td>
+                        <td>
+                            <img src="/assets/cover/"<?= $film["cover"] ?> " alt="Image" width="35px" height="50px">
+                        </td>
+                        <td>
+                            <?= $film["nama_film"] ?>
+                        </td>
+                        <td>
+                            <?= $film["nama_genre"] ?>
+                        </td>
+                        <td>
+                            <?= $film["duration"] ?>
+                        </td>
+
+                        <td>
+                        <a href="/film/update/<?= encryptUrl($film["id"]); ?>" class="btn btn-success">Update</a>
+                        <a class="btn btn-danger" onclick="return confirmDelete()">Delete</a>
+                        
+                        </td>
+                        
+                    </tr>
+                <?php endforeach; ?>
+
+            </table>
+        </div>
+
+    </div>
+</div>
+
+<!-- import JS -->
+<script src="/img/js/bootstrap.min.js"></script>
+  </div>
+</div>
+<script>
+    function confirmDelete() {
+        swal({
+                title: "Apakah Anda yakin?",
+                text: "setelah dihapus! data anda akan benar-benar hilang!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+            .then((willDelete) => {
+                if (willDelete) {
+
+                    window.location.href = "/film/destroy/<?= encryptUrl($film['id']) ?>";
+
+                } else {
+                    swal("Data tidak jadi dihapus!");
+                }
+            });
+    }
+</script>
+
+
+<?= $this->endSection(); ?>
--- CodeIgniter/MI4BApp/app/Views/film/semuaFilm.php
+++ CodeIgniter/MI4BApp/app/Views/film/semuaFilm.php
@@ -1,33 +1,24 @@
 <?= $this->extend('layout/page_layout') ?>
 
 <?= $this->section('content') ?>
-   
-<div class="container">
-  <h1>Beranda</h1>
+
+  <h1>Data Film</h1>
         <div class="row">
-            <?php foreach ($semuafilm as $film) : ?>
+            <?php foreach ($semuafilm as $film): ?>
                 <div class="col-md-3">
                     <div class="card">
-                        <img src="/assets/cover/<?= $film["cover"] ?>"
-                        class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                <?= $film["nama_film"] ?>
-                            </h5>
-                            <p class="card-text">
-                                <?= $film["nama_genre"] ?> ||
-                                <?= $film["duration"] ?>
-                            </p>
+                        <img src="/assets/cover/<?= $film["cover"] ?>"class="card-img-top">
+                        <div class="card-body"><h5 class="card-title"><?= $film["nama_film"] ?></h5>
+                            <p class="card-text"><?= $film["nama_genre"] ?> ||<?= $film["duration"] ?></p>
                             <a href="#" class="btn btn-info">Detail</a>
                             <a href="#" class="btn btn-success">Update</a>
                             <a href="#" class="btn btn-warning">Delete</a>
                         </div>
-
                     </div>
-
                 </div>
             <?php endforeach; ?>
         </div>
-</div>
+
+<script src="/img/js/bootstrap.min.js"></script>
 
 <?= $this->endSection() ?>
